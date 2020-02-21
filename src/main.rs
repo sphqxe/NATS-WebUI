05f28ba4f2057765af87661e2bb9e4da947b0ce8
Theodore Lee <sphqxelzlt@gmail.com>
2020-02-21 17:12:15 +0800

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use log::Level;
+
+#[tokio::main]
+async fn main() {
+    simple_logger::init_with_level(Level::Info).unwrap();
+    let log = warp::log("web");
+
+    let static_content_route = warp::any()
+      .and(warp::fs::dir("web\\dist"))
+      .with(log);
+
+    let api_route = warp::path("api")
+      .map(|| "API Test");
+
+    let route = static_content_route.or(api_route);
+
+    warp::serve(route)
+      .run(([0, 0, 0, 0], 80))
+      .await;
 }
